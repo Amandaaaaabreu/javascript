5d18f1f3687eea090e24da9a0b0ac937a16b8f61
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-10 19:52:54 -0300

    att js

---
+++ condicoes1.js
@@ -0,0 +1,7 @@
+var veloc = 70.5
+
+console.log(`A velocidade do seu carro é ${veloc}km/h `)
+if(veloc > 60){ // condição simples
+    console.log(`Você ultrapassou a velocidade e foi MULTADO.`)
+}
+console.log(`Dirija sempre com cuidado!`)
