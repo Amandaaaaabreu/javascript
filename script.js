78aec37efe007ddee56bba25c5617cd08b1b6ac2
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-13 11:39:20 -0300

    att js

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('manha')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 22
  
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora <12){
-        // bomdia
+       
         img.src = 'modelo/fotomanha.png'
-    } else if(hora >= 12 && hora <18){
-        //boa tarde
+        document.body.style.background = '#EAE3D1'
+
+    } else if(hora >= 12 && hora <= 18){
+      
         img.src ='modelo/fototarde.png'
+        document.body.style.background = '#D88971'
     }else{
-        //boa noite
+        
         img.src = 'modelo/fotonoite.png'
+        document.body.style.background = '#455461'
     }
 }
 
