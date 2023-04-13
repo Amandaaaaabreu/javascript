3d6190bb62efb0ae24a9ec2ffcbaaf6b632f1fc2
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-13 11:44:31 -0300

    att exer hora do dia

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('manha')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 15
+    var hora = data.getHours()
+   
  
     msg.innerHTML = `Agora são ${hora} horas.`
 
