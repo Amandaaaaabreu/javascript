cb45efc98ee0ac0da199eb647a515775f33e9548
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-14 10:30:28 -0300

    att js verificador_Idade

--- verificador_Idade/verificador.js
+++ verificador_Idade/verificador.js
@@ -9,12 +9,43 @@ function verificar(){
         var sexo = document.getElementsByName('radsex')
         var idade = ano - Number(formAno.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
         if(sexo[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                // crianca
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-bebe-m.png')
+            }else if(idade >= 10 && idade <= 21){
+                // jovem
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-jovem-m.png')
+            }else if(idade > 21 && idade < 50){
+                // adulto
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-adulto-m.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-idoso-m.png')
+            }
+                
         } else if(sexo[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                // crianca
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-bebe-f.png')
+            }else if(idade >= 10 && idade <= 21){
+                // jovem
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-jovem-f.png')
+            }else if(idade > 21 && idade < 50){
+                // adulto
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-adulto-f.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'Fotos Ex15 Curso JS/foto-idoso-f.png')
+            }
         }
         resultado.style.textAlign = 'center'
         resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        resultado.appendChild(img)
+
     }
 }
