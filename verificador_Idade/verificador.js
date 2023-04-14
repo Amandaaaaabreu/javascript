51507a6336f2ca85b44b4b7a858b8df3ed758d8b
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-14 10:10:40 -0300

    attt js verif de idade

--- verificador_Idade/verificador.js
+++ verificador_Idade/verificador.js
@@ -3,9 +3,18 @@ function verificar(){
     var ano = data.getFullYear()
     var formAno = document.getElementById('txtano')
     var resultado = document.getElementById('resultado')
-    if(formAno.value.length == 0 || formAno > ano){
+    if(formAno.value.length == 0 || Number(formAno.value) > ano){
         window.alert('Verifique os dados e tente novamente!')
     }else{
-        window.alert('Tudo certo!')
+        var sexo = document.getElementsByName('radsex')
+        var idade = ano - Number(formAno.value)
+        var genero = ''
+        if(sexo[0].checked){
+            genero = 'Homem'
+        } else if(sexo[1].checked){
+            genero = 'Mulher'
+        }
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
