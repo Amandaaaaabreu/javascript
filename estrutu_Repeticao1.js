b492c35f1d14d5187d30c063f338aef736bafe10
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-19 14:25:51 -0300

    att estrutu_Repeticao

--- estrutu_Repeticao1.js
+++ estrutu_Repeticao1.js
@@ -1,12 +1,12 @@
 /* 
-var cont = 1
+var cont = 1 // na estrutura while o teste logico é feito no inicio
 while(cont <= 3){
     console.log(`Passo ${cont}`)
     cont++ 
 }
 */
 
-var cont = 1
+var cont = 1  // na estrutura do while o teste logico é feito no final
 do {
     console.log(`Passo ${cont}`)
     cont++ 
---
+++ estrutu_Repeticao2.js
@@ -0,0 +1,8 @@
+/* estrutura de repeticao com variavel de controle
+1- é feito a inicialização
+2- é feito o teste logico
+3- é feito o incremento
+
+for(inicio;teste;incr){}
+
+*/
