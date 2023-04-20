c6c002af3226b63a67879d1a3307a57fdfe45621
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-20 12:27:13 -0300

    att tabuada js

---
+++ verificador_Idade/script.js
@@ -0,0 +1,22 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Digita um número')
+        
+    }else{
+        let n = Number(num.value) // transformando o que foi escrito na caixa de texto se string para int
+        let c = 1 
+        tab.innerHTML = '' //limpando a minha area de resultado.
+        while(c <= 10){
+           let item = document.createElement('option')
+           item.text = ` ${n} x ${c} = ${n*c} `
+           tab.appendChild(item) // add um item ao filho
+           c++
+        }
+
+    }
+
+    
+}
