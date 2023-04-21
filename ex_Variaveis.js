a360433863944a3713fb2612f6bff106908d1f59
Amanda Abreu <103958998+Amandaaaaabreu@users.noreply.github.com>
2023-04-21 18:59:59 -0300

    att array

---
+++ ex_Variaveis.js
@@ -0,0 +1,23 @@
+let num = [2,5,1,3,7]
+
+for(let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+//num.push(3)
+//num.sort()
+
+//num[6] = 7 // estou criando mais um elemento , add o indice (6) e colocando o valor 7 dentro do elemento
+
+//num.push(9) //metodo interno que vai acrescentar valores a um array. 
+
+//num.length // para saber o comprimento de um array
+
+//num.sort() // coloca os elementos em ordem crescente
+
+
+//console.log(num)
+
+//console.log(`O vetor tem ${num.length} posições`) // me fala quantos elementos tem no meu array
+
+//console.log(`O primeiro valor do array é ${num[0]}`) // me fala qual valor do conteudo dentro do elemento solicitado dentro do colchete
